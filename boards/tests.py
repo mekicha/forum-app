1bd3e7fe13835e5725c78af1915f48c8bf839ce0
Emeka Icha <emeka.icha@gmail.com>
2018-02-10 07:10:45 +0300

    add basic test for home page

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from .views import home
+
+class HomeTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_home_url_resolves_home_view(self):
+		view = resolve('/')
+		self.assertEqual(view.func, home)
