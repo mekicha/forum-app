e86b8ca487c5aa0af32d98b37f4f973b9fe7c515
Emeka Icha <emeka.icha@gmail.com>
2018-02-10 06:58:43 +0300

    added view template for homepage

--- boards/views.py
+++ boards/views.py
@@ -1,15 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Board
 
 def home(request):
   boards = Board.objects.all()
-  board_names = list()
 
-  for board in boards:
-  	board_names.append(board.name)
-
-  response_html = '<br>'.join(board_names)
-
-  return HttpResponse(response_html)
+  return render(request, 'home.html', {'boards': boards})
 
