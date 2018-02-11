203cdb75940bcef086d4219dfaaf044c281250e1
Emeka Icha <emeka.icha@gmail.com>
2018-02-11 19:50:25 +0300

    added tests for board topics view

--- boards/tests.py
+++ boards/tests.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 class HomeTests(TestCase):
 	def test_home_view_status_code(self):
@@ -13,3 +14,22 @@ class HomeTests(TestCase):
 	def test_home_url_resolves_home_view(self):
 		view = resolve('/')
 		self.assertEqual(view.func, home)
+
+
+class BoardTopicsTests(TestCase):
+	def setUp(self):
+		Board.objects.create(name='Django', description='Django board.')
+
+	def test_board_topics_view_success(self):
+		url = reverse('board_topics', kwargs={'pk': 1})
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+		# test that view function is resolved correctly
+		view = resolve('/boards/1/')
+		self.assertEqual(view.func, board_topics)
+
+	def test_board_topics_404(self):
+		url = reverse('board_topics', kwargs={'pk': 99})
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)	
--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Board
 
@@ -8,6 +9,6 @@ def home(request):
   return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
-	board = Board.objects.get(pk=pk)
+	board = get_object_or_404(Board, pk=pk)
 	return render(request, 'topics.html', {'board': board})
 
