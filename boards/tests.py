3457434bd9c8ee029dbb8d0dd33629be7e5a25e3
Emeka Icha <emeka.icha@gmail.com>
2018-02-12 00:07:02 +0300

    added tests for new topic view

--- boards/tests.py
+++ boards/tests.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 
-from .views import home, board_topics
+from .views import home, board_topics, new_topic
 from .models import Board
 
 class HomeTests(TestCase):
@@ -46,3 +46,28 @@ class BoardTopicsTests(TestCase):
 		response = self.client.get(board_topics_url)
 		homepage_url = reverse('home')
 		self.assertContains(response, 'href="{0}"'.format(homepage_url))
+
+
+class NewTopicTests(TestCase):
+	def setUp(self):
+		Board.objects.create(name='Django', description='Django board.')
+
+	def test_new_topic_view_success_status_code(self):
+		url = reverse('new_topic', kwargs={'pk': 1})
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_new_topic_view_404(self):
+		url = reverse('new_topic', kwargs={'pk': 199})
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+	def test_new_topic_url_resolves_new_topic_view(self):
+		view = resolve('/boards/1/new/')
+		self.assertEqual(view.func, new_topic)
+
+	def test_new_topic_view_contains_board_topics_link(self):
+		new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+		board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+		response = self.client.get(new_topic_url)
+		self.assertContains(response, 'href="{0}"'.format(board_topics_url))
