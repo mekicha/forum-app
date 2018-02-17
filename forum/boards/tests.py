9310cc5e537b45c55654ebef1c27f60793a09c98
Emeka Icha <emeka.icha@gmail.com>
2018-02-17 12:10:48 +0300

    add tests for new topic form

--- forum/boards/tests.py
+++ forum/boards/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm 
 
 class HomeTests(TestCase):
 	def setUp(self):
@@ -101,7 +102,9 @@ class NewTopicTests(TestCase):
 	def test_new_topic_invalid_post_data(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
 		response = self.client.post(url, {})
+		form = response.context.get('form')
 		self.assertEqual(response.status_code, 200)
+		self.assertTrue(form.errors)
 
 	def test_new_topic_invalid_post_data_empty_fields(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
@@ -113,3 +116,10 @@ class NewTopicTests(TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertFalse(Topic.objects.exists())
 		self.assertFalse(Post.objects.exists())
+
+	def test_contains_form(self):
+		url = reverse('new_topic', kwargs={'pk': 1})
+		response = self.client.get(url)
+		form = response.context.get('form')
+		self.assertIsInstance(form, NewTopicForm)
+
