b6c30dd065a18ceafc4cbd6896ae71976faba86b
Emeka Icha <emeka.icha@gmail.com>
2018-02-17 13:19:24 +0300

    add tests for signup view

--- forum/accounts/tests.py
+++ forum/accounts/tests.py
@@ -1,3 +1,16 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from .views import signup
+
+class SignupTests(TestCase):
+	def test_signup_status_code(self):
+		url = reverse('signup')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_signup_url_resolves_view(self):
+		view = resolve('/signup/')
+
+		self.assertEqual(view.func, signup)
