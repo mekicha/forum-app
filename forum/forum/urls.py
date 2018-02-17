0a45285146a1ab8b2a6c1cf667fd160649494c4d
Emeka Icha <emeka.icha@gmail.com>
2018-02-17 13:06:32 +0300

    add accounts app

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from boards import views
+from accounts import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', auth_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
