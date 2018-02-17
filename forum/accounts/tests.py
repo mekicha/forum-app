2aeaccff352e6c922337784e16a2cb77803955e4
Emeka Icha <emeka.icha@gmail.com>
2018-02-17 17:19:12 +0300

    add tests for successful signup

--- forum/accounts/tests.py
+++ forum/accounts/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
@@ -5,12 +6,44 @@ from django.test import TestCase
 from .views import signup
 
 class SignupTests(TestCase):
-	def test_signup_status_code(self):
+	def setUp(self):
 		url = reverse('signup')
-		response = self.client.get(url)
-		self.assertEqual(response.status_code, 200)
+		self.response = self.client.get(url)
+
+	def test_signup_status_code(self):
+		self.assertEqual(self.response.status_code, 200)
 
 	def test_signup_url_resolves_view(self):
 		view = resolve('/signup/')
 
 		self.assertEqual(view.func, signup)
+
+	def test_csrf(self):
+		self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+	def test_contains_form(self):
+		form = self.response.context.get('form')
+		self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTests(TestCase):
+	def setUp(self):
+		url = reverse('signup')
+		data = {
+			'username': 'johnson',
+			'password1': 'boardtest2020',
+			'password2': 'boardtest2020'
+		}
+		self.response = self.client.post(url, data)
+		self.home_url = reverse('home')
+
+	def test_redirection(self):
+		self.assertRedirects(self.response, self.home_url)
+
+	def test_user_creation(self):
+		self.assertTrue(User.objects.exists())
+
+	def test_user_authentication(self):
+		response = self.client.get(self.home_url)
+		user = response.context.get('user')
+		self.assertTrue(user.is_authenticated)
